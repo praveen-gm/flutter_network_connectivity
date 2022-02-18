e4b78a09d0fef9db152e1e0a04a39ed7b6dc53bd
Praveen G <gmpraveen321@gmail.com>
2022-02-18 19:55:32 +0530

    setting up internet address lookup

--- lib/flutter_network_connectivity.dart
+++ lib/flutter_network_connectivity.dart
@@ -9,6 +9,8 @@ import 'package:flutter/services.dart';
 /// false - Not Connected to Network
 
 class FlutterNetworkConnectivity {
+  static const _defaultLookUpUrl = 'example.com';
+
   /// Method Channel to Check if Network is Available
   /// To check one time call
   static const MethodChannel _methodChannel = MethodChannel(
@@ -17,6 +19,34 @@ class FlutterNetworkConnectivity {
   /// Event Channel to get Stream of Network Status
   static const EventChannel _eventChannel = EventChannel(
       'com.livelifedev.flutter_network_connectivity/network_status');
+
+  /// URL to lookup for to check network connection availability
+  late String _lookUpUrl;
+
+  /// check if internet is available using this bool
+  bool isInternetAvailable = false;
+
+  /// add custom url [lookUpUrl] to lookup instead of [_defaultLookUpUrl]
+  FlutterNetworkConnectivity({String? lookUpUrl}) {
+    _lookUpUrl = lookUpUrl ?? _defaultLookUpUrl;
+
+    _init();
+  }
+
+  /// Registers Network Connectivity Listener from Native
+  void _init() async {
+    /// Listen for Network Connectivity Status
+    _getNetworkConnectivityStatusStream().listen((isNetworkConnected) {
+      _checkConnection();
+    });
+
+    await _registerNetworkConnectivityListener();
+    await _checkConnection();
+  }
+
+  Future unregisterListener() async {
+    await _unregisterNetworkConnectivityListener();
+  }
 
   /// Checks if Network is Available
   ///
@@ -31,6 +61,8 @@ class FlutterNetworkConnectivity {
   /// Register Listener to check for Network Stream
   /// Must be unregistered if not in use
   /// registering only on android as iOS makes use of NetworkMonitor
+  @Deprecated(
+      'This Method will be deprecated on the next release, will be registered internally')
   Future registerNetworkListener() async {
     if (Platform.isAndroid) {
       await _methodChannel.invokeMethod("registerNetworkStatusListener");
@@ -39,7 +71,21 @@ class FlutterNetworkConnectivity {
 
   /// Unregister Listener when not in use
   /// unregister only on android as iOS makes use of NetworkMonitor
+  @Deprecated(
+      'This method will be deprecated on the next release, will be unregistered internally')
   Future unregisterNetworkListener() async {
+    if (Platform.isAndroid) {
+      await _methodChannel.invokeMethod("unregisterNetworkStatusListener");
+    }
+  }
+
+  Future _registerNetworkConnectivityListener() async {
+    if (Platform.isAndroid) {
+      await _methodChannel.invokeMethod("registerNetworkStatusListener");
+    }
+  }
+
+  Future _unregisterNetworkConnectivityListener() async {
     if (Platform.isAndroid) {
       await _methodChannel.invokeMethod("unregisterNetworkStatusListener");
     }
@@ -49,7 +95,31 @@ class FlutterNetworkConnectivity {
   /// Start Listening on [initState] then proceed to [registerNetworkListener]
   /// true - Network connection is available
   /// false = Network connection is not available
+  @Deprecated('This method will be removed on the next released')
   Stream<bool> getNetworkStatusStream() {
     return _eventChannel.receiveBroadcastStream().cast<bool>();
+  }
+
+  Stream<bool> _getNetworkConnectivityStatusStream() {
+    return _eventChannel.receiveBroadcastStream().cast<bool>();
+  }
+
+  Future _checkConnection() async {
+    isInternetAvailable = await isInternetConnectionAvailable();
+  }
+
+  Future<bool> isInternetConnectionAvailable() async {
+    try {
+      final result = await InternetAddress.lookup(_lookUpUrl);
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        isInternetAvailable = true;
+      } else {
+        isInternetAvailable = false;
+      }
+    } on SocketException catch (_) {
+      isInternetAvailable = false;
+    }
+
+    return isInternetAvailable;
   }
 }
